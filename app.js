1eb3d897d503df82e7087ab9e7ea2f8be2252987
sergio <snunezmeneses@gmail.com>
2020-11-07 00:19:02 +0100

    added index;js router; updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const createError = require('http-errors');
 const express = require('express');
 const app = express();
 
-// test server and router connection
-app.use('/', function(req, res, next) {
-  res.send({ status: 'connected!', router: 'app.js' });
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(error, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
+
+  // render error view
+  res.status(error.status || 500);
+  res.send(res.locals);
 });
 
 module.exports = app;

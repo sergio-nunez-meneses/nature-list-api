b8cde56c4727471787e6a44ed1230f78f45df93f
sergio <snunezmeneses@gmail.com>
2020-11-07 02:08:22 +0100

    added include/exclude category in url, and more elements to animals array

--- routes/animals.js
+++ routes/animals.js
@@ -20,9 +20,14 @@ router.get('/', ash(async function(req, res, next) {
     butterflies: 9,
     fishes: 29,
   }
+  const category = {
+    include: 1,
+    exclude: 0
+  };
+  const page = 15;
   let animals = [], animal = {};
 
-  const response = await fetch('https://www.faune-france.org/index.php?m_id=1351&content=last_observations_by_page&home=1&backlink=skip&p_c=1&p_cc=-1&sp_DateSynth=' + date + '&sp_DChoice=offset&sp_DOffset=5&sp_SChoice=category&sp_Cat[never]=1&sp_Cat[veryrare]=1&sp_Cat[rare]=1&sp_Cat[unusual]=0&sp_Cat[escaped]=0&sp_Cat[common]=0&sp_Cat[verycommon]=0&sp_PChoice=canton&sp_cC=11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100000&sp_FChoice=list&sp_FGraphFormat=auto&sp_FMapFormat=yes&sp_FDisplay=DATE_PLACE_SPECIES&sp_FOrder=ALPHA&sp_FOrderListSpecies=ALPHA&sp_FListSpeciesChoice=DATA&sp_FOrderSynth=ALPHA&sp_FGraphChoice=DATA&sp_DFormat=DESC&sp_FAltScale=250&sp_FAltChoice=DATA&sp_FExportFormat=XLS&sp_tg=' + id.birds + '&mp_current_page=1');
+  const response = await fetch('https://www.faune-france.org/index.php?m_id=1351&content=last_observations_by_page&home=1&backlink=skip&p_c=1&p_cc=-1&sp_DateSynth=' + date + '&sp_DChoice=offset&sp_DOffset=5&sp_SChoice=category&sp_Cat[never]=1&sp_Cat[veryrare]=' + category.include + '&sp_Cat[rare]=' + category.include + '&sp_Cat[unusual]=' + category.include + '&sp_Cat[escaped]=' + category.exclude + '&sp_Cat[common]=' + category.include + '&sp_Cat[verycommon]=' + category.include + '&sp_PChoice=canton&sp_cC=11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100000&sp_FChoice=list&sp_FGraphFormat=auto&sp_FMapFormat=yes&sp_FDisplay=DATE_PLACE_SPECIES&sp_FOrder=ALPHA&sp_FOrderListSpecies=ALPHA&sp_FListSpeciesChoice=DATA&sp_FOrderSynth=ALPHA&sp_FGraphChoice=DATA&sp_DFormat=DESC&sp_FAltScale=250&sp_FAltChoice=DATA&sp_FExportFormat=XLS&sp_tg=' + id.birds + '&mp_current_page=' + page + '');
   let data = await response.json();
 
   if (typeof data !== 'object') {
@@ -44,9 +49,12 @@ router.get('/', ash(async function(req, res, next) {
       animals.push({
         id: data[i].listSubmenus[j].listObservation[0].species_array.id,
         name: data[i].listSubmenus[j].listObservation[0].species_array.name,
+        latin_name: data[i].listSubmenus[j].listObservation[0].species_array.latin_name,
         image: get_image,
+        category: data[i].listSubmenus[j].listObservation[0].species_array.rarity,
         latitude: data[i].listSubmenus[j].listObservation[0].lat,
-        longitude: data[i].listSubmenus[j].listObservation[0].lon
+        longitude: data[i].listSubmenus[j].listObservation[0].lon,
+        remarks: data[i].listSubmenus[j].listObservation[0].remarks[0].content.replace(/(<([^>]+)>)/gi, "")
       });
     }
   }
